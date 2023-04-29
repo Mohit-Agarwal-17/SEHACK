e15b22458db86aa6e81bc280a4e518590ed478e9
Mohit-Agarwal-17 <117508996+Mohit-Agarwal-17@users.noreply.github.com>
2023-04-29 21:38:03 +0530

    Delete routes directory

--- routes/taskRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { protect } = require("../middleware/authMiddleware");
-const { addTask, deleteTask } = require("../controllers/taskController");
-
-router.post("/", addTask)
-router.delete("/", deleteTask)
-
-module.exports = router;
